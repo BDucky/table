135ebfc97df9eabfcd00d848f7ed4fc08b11c4d2
Binhle <binh.le@quano6.com>
2023-07-28 03:29:30 +0700

    briefly done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import CollapsibleTable from './components/table';
+import FeeTable from './components/table';
 
 function App() {
   return (
     <div className="App">
-      <CollapsibleTable header="Header" />
+      <FeeTable />
     </div>
   );
 }
